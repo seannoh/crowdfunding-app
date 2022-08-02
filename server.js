c336581ba05d64ece806884bbf0d0a1b94667ad7
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-02 16:26:03 -0500

    set up handlebars files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 
+const exphbs = require ('express-handlebars')
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -22,6 +24,8 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.set('viewengine', 'handlebars')
 
 app.use(routes);
 
